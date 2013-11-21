c5d71b7b720ccaeff43532c02a8277844a65a4de
Bhawani Shanker <bhawani_s@trigent.com>
2013-11-21 19:18:15 +0530

    Soap web service call using php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php phpinfo() ?>
+<?php 
+$WSDL_PATH = '';
+$soap_obj = new soapclient($WSDL_PATH); 
+
+$params = array();
+$response =  $soap_obj->WebServiceMethodNameWillComeHere($params);
+
+if (is_soap_fault($response)) {
+    return false;   
+}else{
+    print_r($response->WebServiceMethodNameWillComeHereReturn);
+}
+?>
